52f6b158dc7632a8b3b8f38b6246d218617efe3b
nadeeraka <nadeerakan@gmail.com>
2019-10-21 17:04:31 +0530

    digonal  differance

--- Fun.js
+++ Fun.js
@@ -1,4 +1,4 @@
-// push
+// my array  push method
 
 const myArry = [1, 2, 3, 4, 5];
 
@@ -156,4 +156,54 @@ const def = (a, b) => {
   arr.push(c, d);
   return arr;
 };
-console.log(def(h, o));
+//console.log(def(h, o));
+
+//very big sum
+// Sample Input
+
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
+
+// 5000000015
+
+let big = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
+const veryBigSum = ar => {
+  return ar.reduce((acc, val) => (acc += val), 0);
+};
+//console.log(veryBigSum(big))
+//Diagonal Difference
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+
+const diagonalDifference = ( arr, arr1, arr2) => {
+  let bigArray = [];
+  let left = [];
+  let right = [];
+  let x = 0;
+  if ((arr, arr1, arr2)) {
+    arr.length == 3 ? bigArray.push(arr) : null;
+    arr1.length == 3 ? bigArray.push(arr1) : null;
+    arr2.length == 3 ? bigArray.push(arr2) : null;
+  }
+  else{
+    return false;
+  }
+  for (let i = 0; i < bigArray.length; i++) {
+    left.push(bigArray[i][i]);
+  }
+  for (let i = bigArray.length - 1; i >= 0; i--) {
+    right.push(bigArray[i][x]);
+    x++;
+  }
+  let r;
+  let l;
+  l = right.reduce((acc, val) => (acc += val), 0);
+  r = left.reduce((acc, val) => (acc += val), 0);
+  return l - r;
+};
+
+console.log(diagonalDifference( [11, 2, 4], [4, 5, 6], [10, 8, -12]));
