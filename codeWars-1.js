25884da80aed05d4f25c40dc07dc3727efeb308e
nadeeraka <nadeerakan@gmail.com>
2019-11-10 00:33:33 +0530

    end of day one

--- codeWars-1.js
+++ codeWars-1.js
@@ -95,8 +95,34 @@ function accum(params) {
   return newArr.join("");
 }
 // Return the number (count) of vowels in the given string.
-
 // We will consider a, e, i, o, and u as vowels for this Kata.
-
 // The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  var vowelsCount = 0;
+  // enter your majic here
+str.split('').map((res)=>{
+  switch (res) {
+      case 'a':
+          vowelsCount++;
+          break;
+          case 'e':
+          vowelsCount++;
+          break;
+          case 'i':
+              vowelsCount++;
+              break;
+              case 'o':
+          vowelsCount++;
+          break;
+          case 'u':
+          vowelsCount++;
+          break;
+    
+      default:
+          break;
+  }
+})
+return vowelsCount;
+}
 
