1b837483ba0022585d36677f26b32810be18978e
nadeeraka <nadeerakan@gmail.com>
2019-11-11 16:16:29 +0530

    need to  optymize added

--- kata-3.js
+++ kata-3.js
@@ -24,4 +24,21 @@ function XO(params) {
     return false;
     
   }
-  
+// TODO
+
+//   Freddy has a really fat left pinky finger, and every time Freddy tries to type an A, he accidentally hits the CapsLock key!
+
+// Given a string that Freddy wants to type, emulate the keyboard misses where each A supposedly pressed is replaced with CapsLock, and return the string that Freddy actually types. It doesn't matter if the A in the string is capitalized or not. When CapsLock is enabled, capitalization is reversed, but punctuation is not affected.
+
+// Examples:
+
+// "The quick brown fox jumps over the lazy dog."
+// -> "The quick brown fox jumps over the lZY DOG."
+
+// "The end of the institution, maintenance, and administration of government, is to secure the existence of the body politic, to protect it, and to furnish the individuals who compose it with the power of enjoying in safety and tranquillity their natural rights, and the blessings of life: and whenever these great objects are not obtained, the people have a right to alter the government, and to take measures necessary for their safety, prosperity and happiness."
+// -> "The end of the institution, mINTENnce, ND dministrTION OF GOVERNMENT, IS TO SECURE THE EXISTENCE OF THE BODY POLITIC, TO PROTECT IT, nd to furnish the individuLS WHO COMPOSE IT WITH THE POWER OF ENJOYING IN Sfety ND TRnquillity their nTURl rights, ND THE BLESSINGS OF LIFE: nd whenever these greT OBJECTS re not obtINED, THE PEOPLE Hve  RIGHT TO lter the government, ND TO Tke meSURES NECESSry for their sFETY, PROSPERITY nd hPPINESS."
+
+// "aAaaaaAaaaAAaAa"
+// -> ""
+
+
