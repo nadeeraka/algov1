9c5c5d1c8cf2c4bb18cade2fd2901ff3056ceea0
nadeeraka <nadeerakan@gmail.com>
2019-11-16 15:53:26 +0530

    done !!

--- 2/List Filtering-kau.js
+++ 2/List Filtering-kau.js
@@ -99,3 +99,65 @@ function digital_root(p) {
   }
   return res;
 }
+//Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// Description:
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+// Rules for a smiling face:
+// -Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// -A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// -Every smiling face must have a smiling mouth that should be marked with either ) or D.
+// No additional characters are allowed except for those mentioned.
+// Valid smiley face examples:
+// :) :D ;-D :~)
+// Invalid smiley faces:
+// ;( :> :} :]
+
+// Example cases:
+
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+// Note: In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same
+// Happy coding!
+
+function countSmileys(arr) {
+
+    let  count = 0
+        for (let i = 0; i < arr.length; i++) {
+           //temp = arr[i].slice(0,1)
+            if(arr[i].slice(0,1)===':' ||arr[i].slice(0,1)===';' )
+            {
+                if(arr[i].slice(1,2)==='D'||arr[i].slice(1,2)===')'  )
+                {
+                    console.log(arr[i])
+                        count += 1
+                }else if((arr[i].slice(1,2)==='~' ||arr[i].slice(1,2)==='-') && (arr[i].slice(2,3)===')'|| arr[i].slice(2,3)==='D'))
+                {
+                    console.log(arr[i])
+                    count += 1
+                }
+                
+            }
+    
+    
+        }
+        return count
+        
+    }
+    
