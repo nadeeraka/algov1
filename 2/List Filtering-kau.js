e62289a5d66e92bd438061ffe059fda6c16af2fc
nadeeraka <nadeerakan@gmail.com>
2019-11-16 08:59:44 +0530

    done

--- 2/List Filtering-kau.js
+++ 2/List Filtering-kau.js
@@ -5,8 +5,8 @@
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 function filter_list(params) {
-    return params.filter((e)=>typeof e == "number")
- }
+  return params.filter(e => typeof e == "number");
+}
 //  Complete the function/method so that it returns the url with anything after the anchor (#) removed.
 
 // Examples:
@@ -14,21 +14,39 @@ function filter_list(params) {
 // // returns 'www.codewars.com'
 // removeUrlAnchor('www.codewars.com#about')
 
-// // returns 'www.codewars.com?page=1' 
+// // returns 'www.codewars.com?page=1'
 // removeUrlAnchor('www.codewars.com?page=1')
 
 function removeUrl(url) {
-    let array = url.split('')
-    let newArray = []
-     for (let i = 0; i < array.length; i++) {
-      if(array[i] === '#')
-         {
-             i = array.length;
-         }else{
-  newArray.push(array[i])
-         }
-     }
-     return newArray.join('')
- 
- }
- 
+  let array = url.split("");
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "#") {
+      i = array.length;
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray.join("");
+}
+//https://www.codewars.com/kata/54ba84be607a92aa900000f1
+//  An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// isIsogram("Dermatoglyphics") == true
+// isIsogram("aba") == false
+// isIsogram("moOse") == false // -- ignore letter case
+
+function isIsogram(params) {
+  let myOb = {};
+  let i = 0;
+  let array = params
+    .toUpperCase()
+    .split("")
+    .map(w => (myOb[w] = w));
+  for (const key in myOb) {
+    if (myOb.hasOwnProperty(key)) {
+      i++;
+    }
+  }
+  return array.length === i;
+}
