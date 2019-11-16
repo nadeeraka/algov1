582a1de671d2cf864775484844f38041a504c289
nadeeraka <nadeerakan@gmail.com>
2019-11-16 11:34:44 +0530

    finished

--- 2/List Filtering-kau.js
+++ 2/List Filtering-kau.js
@@ -50,3 +50,52 @@ function isIsogram(params) {
   }
   return array.length === i;
 }
+
+// In this kata, you must create a digital root function.
+// A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+// Here's how it works:
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+function digital_root(p) {
+  let res = 0;
+  let sn = false;
+
+  const reduceNum = p => {
+    let str = "" + p;
+    str;
+    const reducer = (acc, current) => parseInt(acc) + parseInt(current);
+    str;
+    res;
+    res = str.split("").reduce(reducer, 0);
+
+    return res;
+  };
+  res = reduceNum(p);
+
+  while (res > 10) {
+    res = reduceNum(res);
+  }
+  return res;
+}
