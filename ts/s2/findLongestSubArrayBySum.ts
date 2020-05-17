4ea22c325c3e74ff4bb86e3ef803612a9e674cee
nadeeraka <nadeerakan@gmail.com>
2020-05-17 18:24:06 +0530

    improve algo

--- ts/s2/findLongestSubArrayBySum.ts
+++ ts/s2/findLongestSubArrayBySum.ts
@@ -1,18 +1,54 @@
-const arr:number[] = [1,2,3,2,3,7,5]
+// const arr:number[] = [1,2,3,2,3,7,5]
+// const ans:number = 12
+// // ! solved
+// const findLagestValInSubArray= (arr:number[], n:number)=>
+// {
+//     let count:number = 0
+//     let itaration:number = 0
+
+//   for (let i = 0; i <arr.length; i++) {
+//       const element = arr[i];
+//       count += element
+//       count
+//       if (count ==n) {
+//           //array slice is not woking properly
+//         return [itaration,i]
+//       }
+//       if (count>n ) {
+//           itaration = i-1
+        
+//           i = itaration
+//           count = 0
+//          // itaration
+          
+//       }
+//   }
+// }
+// console.log(findLagestValInSubArray(arr,ans))
+
+// ! version 2
+
+const arr:number[] = [10,1,2,3,2,3,7,5,1,1,1,1,1,1,1,1,1,1,1,1]
 const ans:number = 12
 // ! solved
 const findLagestValInSubArray= (arr:number[], n:number)=>
 {
     let count:number = 0
     let itaration:number = 0
+    let a = []
+    
 
   for (let i = 0; i <arr.length; i++) {
       const element = arr[i];
       count += element
-      count
+      //count
       if (count ==n) {
           //array slice is not woking properly
-        return [itaration,i]
+        a.push({itaration,i,val:i-itaration})
+        itaration = i-1
+        
+        i = itaration
+        count = 0
       }
       if (count>n ) {
           itaration = i-1
@@ -23,5 +59,7 @@ const findLagestValInSubArray= (arr:number[], n:number)=>
           
       }
   }
+ return a.sort((a,b)=>a.val-b.val)[a.length-1]
+  
 }
 console.log(findLagestValInSubArray(arr,ans))
