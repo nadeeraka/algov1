daf8d3b8f86fba4a2ad1cd4e3015d47a76661182
nadeeraka <nadeerakan@gmail.com>
2020-05-18 19:42:57 +0530

    first duplicate add

---
+++ ts/s1/firstDuplicate/1.ts
@@ -0,0 +1,24 @@
+const arr: number[] = [1, 2, 3, 2, 1, 2];
+const arr2: number[] = [1, 3, 1, 2, 4, 5];
+
+
+const value = 5;
+
+const findFirstDuplicate = (arr: number[], n?: number): number => {
+  let obj = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+   
+    if (obj.includes(element)) {
+       obj
+      return element;
+    }
+    obj.push(element)
+ 
+  }
+  
+
+  return -1;
+};
+
+console.log(findFirstDuplicate(arr2,value))
