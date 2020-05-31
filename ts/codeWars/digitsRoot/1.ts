2e2965764c76daa9c2951a0e2d5919f4a04fba22
Nimantha Nadeeraka <nadeerakan@gmail.com>
2020-05-31 18:42:14 +0530

    todo

---
+++ ts/codeWars/digitsRoot/1.ts
@@ -0,0 +1,68 @@
+// In this kata, you must create a digital root function.
+
+// A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+
+// Here's how it works:
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+
+// digital_root(16)
+// => 1 + 6
+// => 7
+
+// digital_root(942)
+// => 9 + 4 + 2
+// => 15 ...
+// => 1 + 5
+// => 6
+
+// digital_root(132189)
+// => 1 + 3 + 2 + 1 + 8 + 9
+// => 24 ...
+// => 2 + 4
+// => 6
+
+// digital_root(493193)
+// => 4 + 9 + 3 + 1 + 9 + 3
+// => 29 ...
+// => 2 + 9
+// => 11 ...
+// => 1 + 1
+// => 2
+
+
+function digital_root(n:number) {
+    //const reducer = (accumulator, currentValue) => accumulator + parseInt(currentValue);
+    let str = n + ''
+    let val = false
+   const recercive = (arr)=>arr.split('').map((a)=>  parseInt(a)).reduce((a,b)=>a+b)
+    
+  console.log(recercive())
+   
+
+}
+
+console.log(digital_root(232134))
---
+++ ts/s1/chunks/1.ts
@@ -0,0 +1,18 @@
+
+// const l = [1,2,3,4]
+
+// function chunk(params:any[],size:number) {
+//     let newArr = []
+//     for (let i = 0; i < params.length; i++) {
+//         let n = []
+//        for (let x = i; x < x+size ; x++) {
+//            n.push(params[x])
+            
+
+//        }
+//        newArr.push(n)
+        
+//     }
+//     return newArr;
+// }
+// console.log(chunk(l,2))
